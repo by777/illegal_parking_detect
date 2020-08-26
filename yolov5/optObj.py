9e227a7590cb6f2a168e8b4013a38bf74f0a325e
by777 <1373953675@qq.com>
2020-08-26 23:04:17 +0800

    xxxx

--- yolov5/optObj.py
+++ yolov5/optObj.py
@@ -1,27 +1,25 @@
 # encoding: utf-8
-# @Time : 20/08/25 23:13
+# @Time : 20/08/26 22:46
 # @Author : Xu Bai
-# @File : 测试.py
+# @File : optObj.py
 # @Desc :
+
 class Opt:
 
     def __init__(self, source='', view_img=False):
         self.weights = 'weights/yolov5s.pt'
-        self.source = ""
+        self.source = "inference/images"
         self.output = 'inference/output'
         self.img_size = 640
         self.conf_thres = 0.4
         self.iou_thres = 0.5
-        self.device = ''
+        self.device = 'cpu'
         self.view_img = True
         self.save_txt = False
         self.classes = None
         self.agnostic_nms = False
         self.augment = False
         self.update = False
+
     def setOpts(**kwargs):
         print(kwargs)
-
-if __name__ == '__main__':
-    opt = Opt()
-    print(opt.weights)
--- yolov5/parking_detection.py
+++ yolov5/parking_detection.py
@@ -4,7 +4,7 @@
 # @File : parking_detection.py
 # @Desc : 使用yolov5检测车辆违停信息
 
-from detect_obj import detect
+from detect import detect
 import torch
 import cv2
 import sys
@@ -13,7 +13,7 @@ import os
 print('Setup complete. Using torch %s %s' %
       (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
 
-video_path = "./inference/car.mp4"
+video_path = "./inference/parking_car.mp4"
 
 
 def get_images():
@@ -24,10 +24,11 @@ def get_images():
     while True:
         ret, frame = cap.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('frame.jpg',frame)
+        cv2.imwrite('inference/images/frame.jpg',frame)
         # cv2.imshow('frame',cv2.imread('frame.jpg'))
         # cv2.waitKey(0)
-        detect('frame.jpg')
+
+
         key_pressed = cv2.waitKey(25)
         # ESC
         if key_pressed == 27:
