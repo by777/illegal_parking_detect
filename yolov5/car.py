fb74d684cb4a1abc38204981c00dc81300ea5814
by777 <1373953675@qq.com>
2020-08-29 21:54:33 +0800

    修改bug

--- yolov5/car.py
+++ yolov5/car.py
@@ -10,7 +10,7 @@ class Car:
         self.last_position = ()
         self.stop_time = 0
         self.id = 0
-        self.img_path = 'inference/images'
+        self.img_path = 'vehicle_imgs'
 
     def __str__(self):
         return " name: " + str(self.name) \
--- yolov5/cv_utils.py
+++ yolov5/cv_utils.py
@@ -8,29 +8,31 @@ import cv2
 import time
 import os
 import sys
-print (os.getcwd())#获得当前目录
-print (os.path.abspath('.'))#获得当前工作目录
-print (os.path.abspath('..'))#获得当前工作目录的父目录
-print (os.path.abspath(os.curdir))#获得当前工作目录
-wd = os.getcwd()
-img_path = 'inference/images/frame.jpg'
-cars_img_path = './vehicle_imgs/cars'
-motors_img_path = './vehicle_imgs/motors'
-print(os.path.exists(motors_img_path))
+
+# wd = os.getcwd()
+img_path = r'inference\images\frame.jpg'
+
+cars_img_path = r'vehicle_imgs\cars'
+
+motors_img_path = r'vehicle_imgs\motors'
+
 img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
 c1 = (1282, 371)
 c2 = (1441, 476)
 color = (0, 255, 255)
 # 裁剪到的自行车
-crop = img[371: 476,1282:1441]
-crop_path = os.path.join(wd, motors_img_path,str(time.time())+'.jpg')
+crop = img[371: 476, 1282:1441]
+crop_path = os.path.join(motors_img_path, str(time.time()).split('.')[0] + '.jpg')
 print(crop_path)
 
+cv2.imwrite(crop_path, crop)
+# r = cv2.imencode('.jpg', crop)[1].tofile(crop_path) #当有中文路径时使用此方法
 
-cv2.imwrite(crop_path,crop)
+
 img = cv2.rectangle(img, c1, c2, color, 4)
 
-# img = cv2.putText(img,text="自行车！！",(c1[0], c1[1]- 2))
+
 cv2.imshow('frame', img)
 cv2.imshow('crop', crop)
 cv2.waitKey(0)
